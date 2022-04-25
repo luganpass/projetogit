0ca202a0539c2009e225c553ccbfe2b847f594d4
sberdine77 <sbsx77xavier@hotmail.com>
2022-04-24 22:28:49 -0300

    Adding vaccine repository

--- src/repositories/MedicationRepository.java
+++ src/repositories/MedicationRepository.java
@@ -1,23 +1,9 @@
 package repositories;
 
-import java.util.ArrayList;
-
-import models.Identifiable;
-import models.Medication;
-
 public class MedicationRepository extends Repository {
 
     public MedicationRepository() {
         super();
     }
 
-
-  public ArrayList<Medication> getMedicationsList() throws Exception {
-        ArrayList<Medication> castedArray = new ArrayList<Medication>();
-        for (Identifiable identifiable : this.getObjects()) {
-            Medication castedMedication = (Medication) identifiable;
-            castedArray.add(castedMedication);
-        }
-        return castedArray;
-    }
 }
---
+++ src/repositories/VaccineRepository.java
@@ -0,0 +1,9 @@
+package repositories;
+
+public class VaccineRepository {
+
+    public VaccineRepository() {
+        super();
+    }
+    
+}
